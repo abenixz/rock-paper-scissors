38af8b775204d9dbdd921dbc328bd5becd2bda54
abenixz <abenixz@gmail.com>
2026-02-11 12:15:01 +0300

    Add a function to calculate the human choice

--- main.js
+++ main.js
@@ -16,3 +16,26 @@ function getComputerChoice() {
     } 
     return chosenWord;
 }  
+
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice from rock, paper or scissors!");
+
+    if (humanChoice === null) {
+        console.log("We will play another time.");
+    } else if (humanChoice === "") {
+        console.log("You didn't input nothing, You can try again");
+    } else if (!isNaN(humanChoice)) {
+        console.log("Enter a valid choice one from Rock, Paper or Scissor and Try again!");
+    } else if (humanChoice === getComputerChoice()) {
+        
+        console.log("You: " + humanChoice);
+        console.log("Computer: " + getComputerChoice());
+        console.log("We are in a tie");
+        
+    } else if ( humanChoice !== getComputerChoice()) {
+        console.log("You: " + humanChoice);
+        console.log("Computer: " + getComputerChoice());
+    }
+    return;
+}
+getHumanChoice()
