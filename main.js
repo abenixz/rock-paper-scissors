2f9555fc8fdbfccf46dde9c6c91b244c4a634c66
abenixz <abenixz@gmail.com>
2026-02-11 12:14:38 +0300

    Add a function to return computer choice

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice() {
+
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+
+    const randomChoice = Math.floor(Math.random() * 3) + 1;
+
+    let chosenWord;
+    if (randomChoice === 1) {
+        chosenWord = rock;
+    } else if (randomChoice === 2) {
+        chosenWord = paper;
+    } else {
+        chosenWord = scissors;
+    } 
+    return chosenWord;
+}  
